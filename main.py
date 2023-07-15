97fee2c4ad86b20a099affd3a2c8bd5093cc44e7
nardone_dj <nardonedon@outlook.com>
2023-07-14 19:35:47 -0500

    Added exception handling to the user input.

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import pandas
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 # print(nato_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_word = input("Enter a word to see it phonetically: ").upper()
-nato_answer = [nato_dict[letter] for letter in user_word]
-print(nato_answer)
+
+def create_word():
+    user_word = input("Enter a word to see it phonetically: ").upper()
+    try:
+        nato_answer = [nato_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Only letters in the Alphabet please.")
+        create_word()
+    else:
+        print(nato_answer)
 
 
+create_word()
